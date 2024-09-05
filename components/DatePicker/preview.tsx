e58cf57ec4d1e7ced516325939787615557751d5
Mafiz <mafizonly@gmail.com>
2024-09-06 01:44:47 +0530

    fixes in custom dates

--- components/DatePicker/preview.tsx
+++ components/DatePicker/preview.tsx
@@ -15,13 +15,14 @@ import {
   addWeeks,
   addYears,
   endOfMonth,
+  setDay,
 } from "date-fns";
 import { FrequencyTypes, RecurrenceTypes } from "@/types/datepicker";
 import "react-day-picker/style.css";
 
 interface CustomRecurrence {
   every: number;
-  type: string;
+  type: FrequencyTypes;
 }
 
 interface PreviewCalenderProps {
@@ -41,60 +42,83 @@ const PreviewCalender: React.FC<PreviewCalenderProps> = ({
   const [currentMonth, setCurrentMonth] = useState<Date>(startDate);
   const [prevMonth, setPrevMonth] = useState<Date>(startDate);
 
-  const generateCustomDates = (currentDate: Date) => {
+  const generateCustomDates = (currentDate: Date): Date[] => {
+    // Return an empty array if the recurrence pattern is not properly defined
     if (!(customRecurrence?.every && customRecurrence?.type)) {
       return [];
     }
 
-    const { every, type } = customRecurrence || {};
-    const dates = [];
+    const { every, type } = customRecurrence;
+    const dates: Date[] = [];
     let lastDate = currentDate;
+
+    // Handle daily recurrence with respect to the last selected date
     if (
       selectedDates.length > 0 &&
-      isAfter(currentDate, selectedDates[selectedDates.length - 1])
+      isAfter(currentDate, selectedDates[selectedDates.length - 1]) &&
+      type === FrequencyTypes.DAY
     ) {
       lastDate = addDays(selectedDates[selectedDates.length - 1], every);
     }
 
     const lastOfMonth = endOfMonth(currentDate);
-    console.log("Current Date", lastDate);
 
-    while (isBefore(lastDate, lastOfMonth)) {
-      switch (type) {
-        case FrequencyTypes.DAY:
-          dates.push(new Date(lastDate));
-          lastDate = add(lastDate, { days: every });
-          break;
-        case FrequencyTypes.WEEK:
-          dates.push(new Date(lastDate));
-          lastDate = addWeeks(lastDate, every);
-          break;
-        case FrequencyTypes.MONTH:
-          if (lastDate.getDate() <= getDaysInMonth(lastDate)) {
-            dates.push(new Date(lastDate));
-          }
-          lastDate = addMonths(lastDate, every);
-          break;
-        case FrequencyTypes.YEAR:
-          dates.push(new Date(lastDate));
-          lastDate = addYears(lastDate, every);
-          break;
+    // Process different recurrence types
+    if (type === FrequencyTypes.DAY) {
+      // Generate daily dates
+      while (isBefore(lastDate, lastOfMonth)) {
+        dates.push(new Date(lastDate));
+        lastDate = addDays(lastDate, every);
+      }
+    } else if (type === FrequencyTypes.WEEK) {
+      let targetDay;
+      // Weekly recurrence logic
+      if (lastDate == startDate) {
+        targetDay = getDay(lastDate);
+      } else {
+        targetDay = getDay(selectedDates[selectedDates.length - 1]);
+        lastDate = selectedDates[selectedDates.length - 1];
+      }
+
+      // Generate weekly dates
+      while (isBefore(lastDate, lastOfMonth)) {
+        dates.push(new Date(lastDate));
+        lastDate = addWeeks(lastDate, every);
+
+        // Ensure the week day matches the target day
+        if (getDay(lastDate) !== targetDay) {
+          lastDate = setDay(lastDate, targetDay, { weekStartsOn: 0 });
+        }
+      }
+    } else {
+      // Monthly or yearly recurrence logic
+      if (lastDate === startDate) {
+        dates.push(new Date(lastDate));
+      } else {
+        const lastSelectedDate = selectedDates[selectedDates.length - 1];
+        let nextDate = lastSelectedDate;
+        if (type === FrequencyTypes.MONTH) {
+          nextDate = addMonths(lastSelectedDate, every);
+        } else {
+          nextDate = addYears(lastSelectedDate, every);
+        }
+        dates.push(new Date(nextDate));
       }
     }
 
-    console.log("dates", dates);
     return dates;
   };
 
-  const generateDates = (targetDate: Date, currentMonth: Date) => {
+  const generateDates = (targetDate: Date, currentMonth: Date): Date[] => {
     const dates: Date[] = [];
     let currentDate = new Date(targetDate);
 
+    // If the target date is before the start date, no dates are generated
     if (isBefore(currentDate, startDate)) {
       return [];
     }
 
-    // run until the current date is in the same month as the current month or the end date is reached
+    // Loop through the current month to generate dates based on the recurrence pattern
     while (
       isSameMonth(currentDate, currentMonth) &&
       (!endDate ||
@@ -102,14 +126,17 @@ const PreviewCalender: React.FC<PreviewCalenderProps> = ({
         isSameDay(currentDate, endDate))
     ) {
       if (recurrencePattern === "DAILY") {
+        // Daily recurrence
         dates.push(new Date(currentDate));
         currentDate = add(currentDate, { days: 1 });
       } else if (recurrencePattern === "WEEKLY") {
+        // Weekly recurrence
         if (getDay(currentDate) === getDay(targetDate)) {
           dates.push(new Date(currentDate));
         }
         currentDate = add(currentDate, { days: 1 });
       } else if (recurrencePattern === "MONTHLY") {
+        // Monthly recurrence
         if (
           currentDate.getDate() === targetDate.getDate() &&
           currentDate.getDate() <= getDaysInMonth(currentDate)
@@ -118,6 +145,7 @@ const PreviewCalender: React.FC<PreviewCalenderProps> = ({
         }
         currentDate = add(currentDate, { days: 1 });
       } else if (recurrencePattern === "YEARLY") {
+        // Yearly recurrence
         if (
           currentDate.getDate() === targetDate.getDate() &&
           currentDate.getMonth() === targetDate.getMonth() &&
@@ -132,7 +160,8 @@ const PreviewCalender: React.FC<PreviewCalenderProps> = ({
     return dates;
   };
 
-  const handleMonthChange = (newMonth: Date) => {
+  const handleMonthChange = (newMonth: Date): void => {
+    // Prevent changing to a month before the previous month or after the end date
     if (
       isBefore(newMonth, prevMonth) ||
       (endDate && isAfter(newMonth, endDate))
@@ -143,11 +172,12 @@ const PreviewCalender: React.FC<PreviewCalenderProps> = ({
     const targetDay = getDay(startDate); // Extract the day of the week from startDate
     let nextMonthDate = startOfMonth(newMonth);
 
-    if (recurrencePattern === RecurrenceTypes.WEEKLY)
+    // Handle different recurrence patterns (weekly, monthly, yearly, custom)
+    if (recurrencePattern === RecurrenceTypes.WEEKLY) {
       while (getDay(nextMonthDate) !== targetDay) {
         nextMonthDate = addDays(nextMonthDate, 1);
       }
-    else if (recurrencePattern === RecurrenceTypes.MONTHLY) {
+    } else if (recurrencePattern === RecurrenceTypes.MONTHLY) {
       nextMonthDate = addMonths(prevMonth, 1);
     } else if (recurrencePattern === RecurrenceTypes.YEARLY) {
       nextMonthDate = addMonths(prevMonth, 12);
@@ -155,27 +185,26 @@ const PreviewCalender: React.FC<PreviewCalenderProps> = ({
     } else if (recurrencePattern === RecurrenceTypes.CUSTOM) {
       nextMonthDate = addMonths(prevMonth, 1);
 
-      console.log("next month date", nextMonthDate);
       const nextDates = generateCustomDates(nextMonthDate);
       setSelectedDates((prev) => [...prev, ...nextDates]);
     }
 
     setPrevMonth(nextMonthDate);
 
+    // If not yearly or custom recurrence, generate regular recurring dates
     if (
       recurrencePattern !== RecurrenceTypes.YEARLY &&
       recurrencePattern !== RecurrenceTypes.CUSTOM
     ) {
       const newDates = generateDates(nextMonthDate, newMonth);
       setSelectedDates((prev) => [...prev, ...newDates]);
-      console.log("selected dates inside change month", selectedDates);
     }
   };
-
   useEffect(() => {
     if (recurrencePattern === RecurrenceTypes.CUSTOM) {
       console.log("custom recurrence");
       if (customRecurrence?.every && customRecurrence?.type) {
+        setSelectedDates([]);
         setSelectedDates(generateCustomDates(startDate));
         setCurrentMonth(startOfMonth(startDate));
         setPrevMonth(startDate);
